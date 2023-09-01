32f6d65bdcfc9a217bdb90c41ee233cd984575c7
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-09-01 21:03:33 +0300

    added titles

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,15 @@
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">
+                Discover & Share
+                <br className="max-md:hidden" />
+                <span className="orange_gradient text-center">
+                    AI-powered Prompts
+                </span>
+            </h1>
+        </section>
+    );
 };
 
 export default Home;
