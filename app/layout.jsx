d9b1c799195701e3d5b98491c96b83425377ec7f
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-09-01 11:10:52 +0300

    created new layout page

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import "@styles/globals.css";
+
+export const metadata = {
+    title: "Sharing usefull prompts",
+    description: "Discover and share AI prompts",
+};
+const RootLayout = () => {
+    return (
+        <html lang="en">
+            <body>
+                <div className="main">
+                    <div className="gradient" />
+                </div>
+                <main className="app">{children}</main>
+            </body>
+        </html>
+    );
+};
+
+export default RootLayout;
