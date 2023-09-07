0626fc458c593db391e4872b324d7b0852bbb4b7
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-09-07 22:07:12 +0300

    fixed the open loop error

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,6 +8,15 @@ import React from "react";
 
 const Nav = () => {
     const isUserLoggedIn = true;
+    const [providers, setProviders] = useState(null);
+    const [toggleDropdown, setToggleDropdown] = useState(false);
+    // useEffect(() => {
+    //     const setProviders = async () => {
+    //         const response = await getProviders();
+    //         setProviders(response);
+    //     };
+    //     setProviders();
+    // }, []);
 
     return (
         <nav className="flex-between w-full mb-16 pt-3">
@@ -29,9 +38,78 @@ const Nav = () => {
                         <Link href="/create-prompt" className="black_btn">
                             Create Post
                         </Link>
+                        <button
+                            type="button"
+                            onClick={signOut}
+                            className="outline_btn"
+                        >
+                            Sign Out
+                        </button>
+                        <Link href="/profile">
+                            <Image
+                                src="/assets/images/logo.svg"
+                                width={37}
+                                height={37}
+                                className="rounded-full"
+                                alt="profile"
+                            />
+                        </Link>
                     </div>
                 ) : (
-                    <></>
+                    <>
+                        {providers &&
+                            Object.values(providers).map((provider) => (
+                                <button
+                                    type="button"
+                                    key={provider.name}
+                                    onClick={() => signIn(provider.id)}
+                                    className="black_btn"
+                                >
+                                    Sign In
+                                </button>
+                            ))}
+                    </>
+                )}
+            </div>
+
+            {/* Mobile Navigation */}
+            <div className="sm:hidden flex relative">
+                {isUserLoggedIn ? (
+                    <div className="flex">
+                        <Image
+                            src="/assets/images/logo.svg"
+                            width={37}
+                            height={37}
+                            className="rounded-full"
+                            alt="profile"
+                            onClick={() => {}}
+                        />
+                        {/* {toggleDropdown && (
+                            <div className="dropdown">
+                                <Link
+                                    href="/profile"
+                                    className="dropdown_link"
+                                    onClick={() => setToggleDropdown(false)}
+                                >
+                                    My Profile
+                                </Link>
+                            </div>
+                        )} */}
+                    </div>
+                ) : (
+                    <>
+                        {providers &&
+                            Object.values(providers).map((provider) => (
+                                <button
+                                    type="button"
+                                    key={provider.name}
+                                    onClick={() => signIn(provider.id)}
+                                    className="black_btn"
+                                >
+                                    Sign In
+                                </button>
+                            ))}
+                    </>
                 )}
             </div>
         </nav>
