a098d0b4adb5b8802803058ada7e69c2d1325972
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-09-07 22:06:56 +0300

    fixed the open loop error

--- components/delete.jsx
+++ components/delete.jsx
@@ -1,7 +1,6 @@
 const Nav = () => {
     const isUserLoggedIn = true;
     const [providers, setProviders] = useState(null);
-    const [toggleDropdown, setToggleDropdown] = useState(false);
 
     useEffect(() => {
         const setProviders = async () => {
@@ -44,46 +43,6 @@ const Nav = () => {
                                 alt="profile"
                             />
                         </Link>
-                    </div>
-                ) : (
-                    <>
-                        {providers &&
-                            Object.values(providers).map((provider) => (
-                                <button
-                                    type="button"
-                                    key={provider.name}
-                                    onClick={() => signIn(provider.id)}
-                                    className="black_btn"
-                                >
-                                    Sign In
-                                </button>
-                            ))}
-                    </>
-                )}
-            </div>
-            {/* Mobile Navigation */}
-            <div className="sm:hidden flex relative">
-                {isUserLoggedIn ? (
-                    <div className="flex gap-3 md:gap-5">
-                        <Image
-                            src="/assets/images/logo.svg"
-                            width={37}
-                            height={37}
-                            className="rounded-full"
-                            alt="profile"
-                            onClick={() => setToggleDropdown((prev) => !prev)}
-                        />
-                        {toggleDropdown && (
-                            <div className="dropdown">
-                                <Link
-                                    href="/profile"
-                                    className="dropdown_link"
-                                    onClick={() => setToggleDropdown(false)}
-                                >
-                                    My Profile
-                                </Link>
-                            </div>
-                        )}
                     </div>
                 ) : (
                     <>
