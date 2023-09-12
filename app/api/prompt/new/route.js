09ae8f9e071a452c0f3d4c1ba573d2d5ba896033
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-09-12 22:56:42 +0300

    updated the API

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,1 +1,9 @@
-export const POST = async (req, res) => {};
+import { connectToDB } from "@utils/database";
+
+export const POST = async (req) => {
+    const { userId, prompt, tag } = await req.json();
+
+    try {
+        await connectToDB();
+    } catch (error) {}
+};
