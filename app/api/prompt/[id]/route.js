7b173027c70bd9082540dcfde2fdb06a525164d9
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-09-17 02:09:21 +0300

    added the patch api to the routes

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -13,3 +13,21 @@ export const GET = async (request, { params }) => {
         return new Response("Failed to fetch all prompts.", { status: 500 });
     }
 };
+//PATCH (edit)
+export const PATCH = async (request, { params }) => {
+    const { prompt, tag } = await request.json();
+    try {
+        await connectToDB();
+
+        const existingPrompt = await prompt.findById(params.id);
+
+        if (!existingPrompt)
+            return new Response("Prompt not found", { status: 404 });
+        existingPrompt.prompt = prompt;
+        existingPrompt.tag = tag;
+        await existingPrompt.save();
+        return new Response(JSON.stringify(existingPrompt), { status: 200 });
+    } catch (error) {
+        return new Response("Failed to fetch all prompts.", { status: 500 });
+    }
+};
