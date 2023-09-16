ac9432ce9621e62b2a11014ed64de678edbf7eb1
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-09-17 00:36:49 +0300

    edited the profile page

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,7 +1,12 @@
+import PromptCard from "./PromptCard";
 import React from "react";
 
-const Profile = () => {
-    return <div>Profile</div>;
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
+    return (
+        <section className="w-full">
+            <h1 className="head_text text-left">{name} Profile</h1>
+        </section>
+    );
 };
 
 export default Profile;
