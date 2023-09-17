24452a0017d3d24f0e90b928e7c6f412457b0fd7
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-09-17 18:04:05 +0300

    added the search and sesarch by

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,6 +1,9 @@
 "use client";
+
 import { useState, useEffect } from "react";
+
 import PromptCard from "./PromptCard";
+
 const PromptCardList = ({ data, handleTagClick }) => {
     return (
         <div className="mt-16 prompt_layout">
@@ -15,9 +18,12 @@ const PromptCardList = ({ data, handleTagClick }) => {
     );
 };
 const Feed = () => {
-    const [searchText, setSearchText] = useState("");
     const [posts, setPosts] = useState([]);
-    const handleSearchChange = (e) => {};
+
+    // Search states
+    const [searchText, setSearchText] = useState("");
+    const [searchTimeout, setSearchTimeout] = useState(null);
+    const [searchedResults, setSearchedResults] = useState([]);
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -27,6 +33,37 @@ const Feed = () => {
         };
         fetchPosts();
     }, []);
+
+    const filterPrompts = (searchtext) => {
+        const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+        return posts.filter(
+            (item) =>
+                regex.test(item.creator.username) ||
+                regex.test(item.tag) ||
+                regex.test(item.prompt)
+        );
+    };
+
+    const handleSearchChange = (e) => {
+        clearTimeout(searchTimeout);
+        setSearchText(e.target.value);
+
+        // debounce method
+        setSearchTimeout(
+            setTimeout(() => {
+                const searchResult = filterPrompts(e.target.value);
+                setSearchedResults(searchResult);
+            }, 500)
+        );
+    };
+
+    const handleTagClick = (tagName) => {
+        setSearchText(tagName);
+
+        const searchResult = filterPrompts(tagName);
+        setSearchedResults(searchResult);
+    };
+
     return (
         <section className="feed">
             <form className="relative w-full flex-center">
@@ -40,7 +77,15 @@ const Feed = () => {
                 />
             </form>
 
-            <PromptCardList data={posts} handleTagClick={() => {}} />
+            {/* All Prompts */}
+            {searchText ? (
+                <PromptCardList
+                    data={searchedResults}
+                    handleTagClick={handleTagClick}
+                />
+            ) : (
+                <PromptCardList data={posts} handleTagClick={handleTagClick} />
+            )}
         </section>
     );
 };
